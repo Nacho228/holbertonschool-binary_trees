74ea896b53c47f2805088e68a86a474bee7e5b08
Nacho228 <6319@holbertonstudents.com>
2023-05-14 11:37:09 -0700

    Insert left added

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,35 +4,31 @@
  * @parent: pointer to the parent node.
  * @value: value to pass to new_left->n.
  * Return: pointer to the new node, null otherwise.
-*/
+ */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_left = malloc(sizeof(binary_tree_t));
-    binary_tree_t *old_left = malloc(sizeof(binary_tree_t));
-    
-    if(parent == NULL || new_left == NULL)
-    {
-        free(new_left);
-        free(old_left);
-        return(NULL);
-    }
-        
-        new_left->parent = parent;
-        new_left->n = value;
-        new_left->right = NULL;
+binary_tree_t *new_left = malloc(sizeof(binary_tree_t));
 
-        if(parent->left != NULL)
-        {
-            old_left->n = parent->left->n;
-            parent->left = new_left;
-            new_left->left = old_left;
-            old_left->parent = new_left;
-        }
-        else
-        {
-            parent->left = new_left;
-            new_left->left = NULL;
-        }
-
-return(new_left);
+if (parent == NULL)
+{
+free(new_left);
+return (NULL);
 }
+
+new_left->parent = parent;
+new_left->n = value;
+new_left->right = NULL;
+
+if (parent->left != NULL)
+{
+new_left->left = parent->left;
+new_left->left->parent = new_left;
+}
+else
+{
+new_left->left = NULL;
+}
+parent->left = new_left;
+return (new_left);
+}
+
