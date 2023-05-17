6e2bd0140d5652fa16efe7d272a7ef6b8426c9aa
4mmZ <andresmaxi111014@gmail.com>
2023-05-16 17:21:01 -0700

    task 2: fix malloc

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,34 +1,32 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_insert_right - inserts a node as right child of another.
- * @parent: pointer to the parent node.
- * @value: value to pass to new_right->n.
- * Return: pointer to the new node, null otherwise.
+ *binary_tree_insert_right -  function that inserts a node
+ * as the left-child of another node
+ *@parent: Is a pointer to the node to insert the left-child in
+ *@value: Is the value to store in the new node
+ *Return: A pointer ti the create node, or NULL on failure
  */
+
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-binary_tree_t *new_right = malloc(sizeof(binary_tree_t));
+	binary_tree_t *root = NULL;
 
-if (!parent)
-{
-free(new_right);
-return (NULL);
-}
+	if (!parent)
+		return (NULL);
+	root = binary_tree_node(parent, value);
+	if (!root)
+	{
+		return (NULL);
+	}
+	root->parent = parent;
+	root->n = value;
+	if (parent->right)
+	{
+		root->right = parent->right;
+		parent->right->parent = root;
+	}
+	parent->right = root;
 
-new_right->parent = parent;
-new_right->n = value;
-new_right->left = NULL;
-
-if (parent->right != NULL)
-{
-new_right->right = parent->right;
-parent->right = new_right;
-new_right->right->parent = new_right;
-}
-else
-{
-new_right->right = NULL;
-parent->right = new_right;
-}
-return (new_right);
+	return (parent->right);
 }
